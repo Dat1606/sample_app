d3abe35d823af416822fcf5e13b1c7b1b36290af
Duc Dat <dat.compativelycomplex@gmail.com>
2018-06-19 08:56:48 +0700

    Chapter 10 Finish user edit, update, index and destroy actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
 
-    protect_from_forgery with: :exception
-
-  def hello
-    render html: "Just kidding"
-  end
+  protect_from_forgery with: :exception
+  include SessionsHelper
 
 end
